f4e80d368300cabbcd480644217c18737f69a5cc
anasilerna <anasmejahad15@alumnes.ilerna.com>
2024-12-09 19:20:15 +0100

    a
    hola.txta

---
+++ src/pp.java
@@ -0,0 +1,9 @@
+package default;
+public class pp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	System.out.println("hola");
+}
---
+++ src/pp/pp.java
@@ -0,0 +1,10 @@
+package pp;
+package default;
+public class pp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	System.out.println("hola");
+}
